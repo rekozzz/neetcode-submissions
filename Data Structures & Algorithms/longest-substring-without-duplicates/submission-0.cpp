a7a5254bd8ce9f469f6677da20ccd578d03a72f6
Patrick Shehata <rekoshehata@gmail.com>
2026-05-02 20:37:11 +0300

    Add: longest-substring-without-duplicates - submission-0

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-0.cpp
@@ -0,0 +1,18 @@
+    class Solution {
+    public:
+        int lengthOfLongestSubstring(string s) {
+            int l = 0;
+            unordered_map<char, int> mp;
+            int maxLen = 0;
+
+            for(int r = 0; r < s.size(); r++){
+                mp[s[r]]++;
+                while(mp[s[r]] > 1){
+                    mp[s[l]]--;
+                    l++;
+                }
+               maxLen = max(maxLen, r-l+1);
+            }
+            return maxLen;
+        }
+    };
