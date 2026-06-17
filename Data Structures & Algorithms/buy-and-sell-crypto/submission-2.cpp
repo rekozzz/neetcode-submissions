ad7c0fd4e8f89892e100753372e262829e0ebd47
Patrick Shehata <rekoshehata@gmail.com>
2026-06-17 23:19:34 +0300

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int max = 0;
+        int min = prices[0];
+    
+        for (int i = 0; i <= prices.size() - 1; i++){
+            if(min > prices[i]){
+                min = prices[i];
+            }
+            int maxCalc = prices[i] - min;
+            if(maxCalc > max){
+                max = maxCalc;
+            }
+        }
+        return max;
+    }
+};
