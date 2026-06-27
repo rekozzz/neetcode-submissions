d879fbaf8b18c408fc93eaaa9f743ec2f26d4e28
Patrick Shehata <rekoshehata@gmail.com>
2026-06-27 15:48:52 +0300

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.cpp
@@ -0,0 +1,36 @@
+    class Solution {
+    public:
+
+       string encode(vector<string>& strs) {
+            string encoded_string = "";
+        for (int i = 0; i < strs.size(); i++){
+            
+            int length = strs[i].length();
+
+            encoded_string += to_string(length) + "#" + strs[i];
+        } 
+        return encoded_string;       
+    }   
+
+        vector<string> decode(string s) {
+            vector <string> strs;
+            
+            int i = 0;
+        while(i != s.length()){
+            string len = "";
+            while (s[i] != '#'){
+            len += s[i];
+            i++;
+        }
+        int length = stoi(len);
+        i++;
+        string word = s.substr(i, length);
+        strs.push_back(word);
+        i += length;
+        }
+        
+        return strs;
+        
+        }
+
+    };
