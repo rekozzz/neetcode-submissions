32ea32a31593ac68367e07d679ed49800a681e37
Patrick Shehata <rekoshehata@gmail.com>
2026-09-13 00:50:30 +0300

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+
+        for(int i = 0; i < nums.size(); i++){
+            int compliment = target - nums[i];
+            if(mp.find(compliment) != mp.end()){
+                return {mp[compliment], i};
+            }
+
+            mp.insert({nums[i], i});
+        }
+        return {};
+    }
+
+};
