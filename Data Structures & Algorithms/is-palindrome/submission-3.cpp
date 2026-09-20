b89127635f6ca4b77be0f8c5d963baf7dc1ca727
Patrick Shehata <rekoshehata@gmail.com>
2026-09-20 15:05:30 +0300

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+       string clean = "";
+
+       for(char c:s){
+        if(isalnum(c)){
+            clean += tolower(c);
+        }
+       }
+
+       for(int i = 0; i < clean.size() / 2; i++){
+        if(clean[i] != clean[clean.size() - i - 1])
+        return false;
+       }
+           return true;     
+    }
+};
