4b72508066bf3766368da1235dee2f9010063f41
Patrick Shehata <rekoshehata@gmail.com>
2026-09-23 23:42:11 +0300

    Add: trapping-rain-water - submission-2

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-2.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+     int l = 0;
+     int r = height.size() - 1;
+     int leftMax = 0;
+     int rightMax = 0;
+     int trap = 0;
+
+     while(l < r){
+        if (height[l] < height[r]){
+            if(height[l] >= leftMax){
+                leftMax = height[l];
+            }
+            else{
+                trap += leftMax - height[l];
+            }
+            l++;
+        }
+        else{
+            if(height[r] >= rightMax){
+                rightMax = height[r];
+            }
+            else{
+                trap += rightMax - height[r];
+            }
+            r--;
+        }
+
+     }
+     return trap;
+    }
+};
